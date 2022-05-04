2e53b6de1160cb7aff6cbda5284303f87040b980
fernandalimaa <fernanda.lima@thebrooklynbrothers.com>
2022-05-03 22:04:03 -0300

    confirm logout page

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ function create() {
 function alertaFucn() {
     alert("Tem certeza? Não é possivel desfazer a matricula!");
 }
+
+
+
+
+function validaCPF(input){
+    const cfpformatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+    if(!checarCPFrepedito(cfpformatado)){
+        mensagem=" CPF digitado não é valido !! "
+    }
+
+    input.setCustmValidity(mensagem)
+}
+function checarCPFrepedito(cpf){
+    const valoresRepetidos = [
+        '00000000000','11111111111', '222222222222','33333333333','44444444444', '55555555555',
+        '66666666666', '77777777777', '88888888888', '99999999999'
+    ]
+    let cpfValido=true
+    valoresRepetidos.forEach(valor => {
+        if(valor ==cpf){
+            cpfValido =false
+        }
+    })
+    return cpfValido
+}
+
     
