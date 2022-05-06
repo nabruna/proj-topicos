3c180ab1c576e80d9b6f638e68c5215780c485cb
0708AnaValois <avaloisgoncalves@gmail.com>
2022-05-05 21:58:50 -0300

    aula 05/05

---
+++ src/aluno/script.js
@@ -0,0 +1,51 @@
+function atividadeViolao() {
+    alert("Violão adicionado");
+}
+
+function atividadeBordado() {
+    alert("Bordado adicionado");
+}
+
+function atividadeGames() {
+    alert("Videogames adicionado");
+}
+
+function atividadeMatematica() {
+    alert("Matemática Avançada adicionada");
+}
+
+function atividadeFisica() {
+    alert("Física Avançada adicionada");
+}
+
+function atividadeModa() {
+    alert("Moda adicionada");
+}
+
+function atividadeFutebol() {
+    alert("Futebol adicionada");
+}
+
+function atividadeBasquete() {
+    alert("Basquete adicionada");
+}
+
+function atividadeVolei() {
+    alert("Vôlei adicionada");
+}
+
+function atividadeFotografia() {
+    alert("Fotografia retirada");
+}
+
+function atividadeHoquei() {
+    alert("Hóquei no Gelo retirada");
+}
+
+function atividadeMusica() {
+    alert("Música retirada");
+}
+
+function atividadeTeatro() {
+    alert("Teatro retirada");
+}
