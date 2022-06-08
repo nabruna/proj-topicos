4e44af61a16506fc178f389b2430f424ac282945
Bruna Negrelli <bruna.negrelli@aoatechpartners.com.br>
2022-06-08 18:24:53 -0300

    update da página de relatórios

--- src/professor/scriptNomes.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $.getJSON("../shared/mockAlunos.json", function (data) {
-    var aluno = "";
-    $.each(data.alunos, function (i, value) {
-      aluno += "<li class='list-group-item'>" + value.nome + "</li>";
-    });
-    $("#listaAlunos").append(aluno);
-  });
-});
---
+++ src/professor/scriptRelatorios.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $.getJSON("../shared/mockAlunos.json", function (data) {
+    var relatorios = "";
+    $.each(data.alunos, function (i, value) {
+        relatorios += "<tr></tr><td>" + value.nome + "</td>"
+
+        var matematica = value.materias[1];
+
+        relatorios += "<td>" + matematica.faltas + "</td>"
+        relatorios += "<td>" + matematica.notas[0] + "</td>"
+        relatorios += "<td>" + matematica.notas[1] + "</td>"
+        relatorios += "<td>" + matematica.notas[2] + "</td>"
+        relatorios += "<td>" + matematica.notas[3] + "</td>"
+
+        media = (matematica.notas[0] + matematica.notas[1] + matematica.notas[2] + matematica.notas[3]) / 4;
+
+        relatorios += "<td>" + media.toFixed(1) + "</td>"
+        
+        if (media < 4) {
+          aprovacao = "<span class='text-danger fw-bold'>Reprovado</span>";
+        } else if  (media < 7) {
+          aprovacao = "<span class='text-warning fw-bold'>Em recuperação</span>";
+        } else {
+          aprovacao = "<span class='text-success fw-bold'>Aprovado</span>";
+        }
+
+        relatorios += "<td>" + aprovacao + "</td>"
+    });
+    $("#faltasAlunos").append(relatorios);
+  });
+});
