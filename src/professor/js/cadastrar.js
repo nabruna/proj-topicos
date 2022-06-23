80dbd4979ca8a87c56e2dfdb28ae1ebcc19add57
Bruna Negrelli <bruna.negrelli@aoatechpartners.com.br>
2022-06-23 13:50:05 -0300

    cadastrando aluno YES

---
+++ src/professor/js/cadastrar.js
@@ -0,0 +1,27 @@
+const renderPage = async () => {
+  const form = document.querySelector("form");
+
+  const cadastrarAluno = async (e) => {
+    e.preventDefault();
+
+    const aluno = {
+      nome: form.nome.value,
+      responsavel: form.responsavel.value,
+      turma: form.turma.value,
+      materias: [],
+      atividades: []
+    };
+
+    await fetch("http://localhost:3000/alunos", {
+      method: "POST",
+      body: JSON.stringify(aluno),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    window.location.replace("/src/professor/index.html");
+  };
+
+  form.addEventListener("submit", cadastrarAluno);
+};
+
+window.addEventListener("DOMContentLoaded", () => renderPage());
--- src/professor/js/index.js
+++ src/professor/js/index.js
@@ -1,162 +1,169 @@
-window.onload = () => {
+const renderPage = async () => {
+  let url = "http://localhost:3000/alunos";
+
+  const res = await fetch(url);
+  const alunos = await res.json();
+
   var main = document.getElementsByTagName("main")[0];
   var btnAlunos = document.getElementById("btnAlunos");
   var btnFrequencia = document.getElementById("btnFrequencia");
 
   function gerarTabela(url) {
-    main.innerHTML = "<h1 class='my-2 fw-bold'>Alunos</h1>";
+    var template = "";
+    var titulo = "<h1 class='my-2 fw-bold'>Alunos</h1>";
 
-    fetch(url)
-      .then((resposta) => {
-        return resposta.json();
-      })
-      .then((alunos) => {
-        listaDealunos = alunos;
+    alunos.forEach((aluno) => {
+      template += `
+      <tr>
+       <td>
+        ${aluno.nome}
+      </td>
+      <td>
+      ${aluno.turma}
+      </td>
+      <td>`;
 
-        //Código de geração da tabela
-        var table = document.createElement("table");
-        table.classList.add("table");
+      for (let i = 0; i < aluno.materias.length; i++) {
+        let itemMateria = "";
+        itemMateria += `<li class="list-group-item">${aluno.materias[i].nomeMateria}</li>`;
+        template += itemMateria;
+      }
 
-        var thead = document.createElement("thead");
-        thead.innerHTML =
-          "<tr><th>Nome</th><th>Turma</th><th>Matérias</th><th>Atividades extras</th><th>Responsável</th></tr>";
-        table.appendChild(thead);
+      template += `
+      </td>
+      <td>`;
 
-        var tbody = document.createElement("tbody");
+      for (let i = 0; i < aluno.atividades.length; i++) {
+        let itemMateria = "";
+        itemMateria += `<li class="list-group-item">${aluno.atividades[i].nomeAtividade}</li>`;
+        template += itemMateria;
+      }
+      template += `
+      </td>
+      <td>
+      ${aluno.responsavel}
+      </td>`;
+      template += `
+      </td>
+      </tr>`;
+    });
 
-        var linhasTabela = alunos.length;
+    var tabela =
+      `
+    <table class="table">
+    <thead>
+    <tr><th>Nome</th><th>Turma</th><th>Matérias</th><th>Atividades extras</th><th>Responsável</th></tr>
+    </thead>
+    <tbody>` +
+      template +
+      `</tbody>
+    </table>`;
 
-        for (var i = 0; i < linhasTabela; i++) {
-          var tr = document.createElement("tr");
-          var aluno = alunos[i];
-
-          //Mostrar nome do aluno
-          var td = document.createElement("td");
-          var txt = document.createTextNode(aluno.nome);
-          td.appendChild(txt);
-          tr.appendChild(td);
-
-          //Mostrar turma
-          var td = document.createElement("td");
-          var txt = document.createTextNode(aluno.turma);
-          td.appendChild(txt);
-          tr.appendChild(td);
-
-          //Mostrar lista de matérias
-          var td = document.createElement("td");
-          const listaMaterias = document.createElement("ul");
-          listaMaterias.classList.add("list-group");
-          for (let i = 0; i < aluno.materias.length; i++) {
-            const itemMateria = document.createElement("li");
-            itemMateria.classList.add("list-group-item");
-            var txt = document.createTextNode(aluno.materias[i].nomeMateria);
-            itemMateria.appendChild(txt);
-            listaMaterias.appendChild(itemMateria);
-            td.appendChild(listaMaterias);
-            tr.appendChild(td);
-          }
-
-          //Mostrar lista de atividades extras
-          var td = document.createElement("td");
-          const listaAtividades = document.createElement("ul");
-          listaAtividades.classList.add("list-group");
-          for (let i = 0; i < aluno.atividades.length; i++) {
-            const itemAtividade = document.createElement("li");
-            itemAtividade.classList.add("list-group-item");
-            var txt = document.createTextNode(
-              aluno.atividades[i].nomeAtividade
-            );
-            itemAtividade.appendChild(txt);
-            listaAtividades.appendChild(itemAtividade);
-            td.appendChild(listaAtividades);
-            tr.appendChild(td);
-          }
-
-          //Mostrar responsável
-          var td = document.createElement("td");
-          var txt = document.createTextNode(aluno.responsavel);
-          td.appendChild(txt);
-          tr.appendChild(td);
-
-          tr.appendChild(td);
-          tbody.appendChild(tr);
-        }
-
-        table.appendChild(tbody);
-
-        main.appendChild(table);
-      });
+    main.innerHTML = titulo + tabela;
   }
 
   btnAlunos.onclick = () => {
-    main.innerHTML = "";
     gerarTabela("http://localhost:3000/alunos");
   };
 
   function gerarFreq(url) {
-    main.innerHTML = "<h1 class='my-2 fw-bold'>Frequência</h1>";
+    var template = "";
+    var titulo = "<h1 class='my-2 fw-bold'>Frequência</h1>";
 
-    fetch(url)
-      .then((resposta) => {
-        return resposta.json();
-      })
-      .then((alunos) => {
-        listaDealunos = alunos;
+    alunos.forEach((aluno) => {
+      template += `
+      <tr>
+       <td>
+        ${aluno.nome}
+      </td>
+      <td>`;
 
-        //Código de geração da tabela
-        var table = document.createElement("table");
-        table.classList.add("table");
+      for (let i = 0; i < aluno.materias.length; i++) {
+        let itemMateria = "";
+        itemMateria += `<li class="list-group-item">${aluno.materias[i].nomeMateria}: ${aluno.materias[i].faltas}</li>`;
+        template += itemMateria;
+      }
+      template += `
+      </td>
+      </tr>`;
+    });
 
-        var thead = document.createElement("thead");
-        thead.innerHTML =
-          "<tr><th>Nome</th><th>Faltas</th></tr>";
-        table.appendChild(thead);
+    var tabela =
+      `
+    <table class="table">
+    <thead>
+    <tr><th>Nome</th><th>Faltas</th></tr>
+    </thead>
+    <tbody>` +
+      template +
+      `</tbody>
+    </table>`;
 
-        var tbody = document.createElement("tbody");
+    main.innerHTML = titulo + tabela;
 
-        var linhasTabela = alunos.length;
+    // main.innerHTML = "<h1 class='my-2 fw-bold'>Frequência</h1>";
 
-        for (var i = 0; i < linhasTabela; i++) {
-            var tr = document.createElement("tr");
-            var aluno = alunos[i];
-  
-            //Mostrar nome do aluno
-            var td = document.createElement("td");
-            var txt = document.createTextNode(aluno.nome);
-            td.appendChild(txt);
-            tr.appendChild(td);
-  
-            //Mostrar lista de matérias
-            var td = document.createElement("td");
-            const listaMaterias = document.createElement("ul");
-            listaMaterias.classList.add("list-group");
-            for (let i = 0; i < aluno.materias.length; i++) {
-              const itemMateria = document.createElement("li");
-              itemMateria.classList.add("list-group-item");
-              var txt = document.createTextNode(aluno.materias[i].nomeMateria);
-              itemMateria.appendChild(txt);
-              var sp = document.createTextNode(": ");
-              itemMateria.appendChild(sp);
-              var num = document.createTextNode(aluno.materias[i].faltas);
-              itemMateria.appendChild(num);
-              listaMaterias.appendChild(itemMateria);
-              td.appendChild(listaMaterias);
-              tr.appendChild(td);
-            }
-  
-            tr.appendChild(td);
-            tbody.appendChild(tr);
-          }
+    // fetch(url)
+    //   .then((resposta) => {
+    //     return resposta.json();
+    //   })
+    //   .then((alunos) => {
+    //     listaDealunos = alunos;
 
-        table.appendChild(tbody);
+    //     //Código de geração da tabela
+    //     var table = document.createElement("table");
+    //     table.classList.add("table");
 
-        main.appendChild(table);
-      });
+    //     var thead = document.createElement("thead");
+    //     thead.innerHTML = "<tr><th>Nome</th><th>Faltas</th></tr>";
+    //     table.appendChild(thead);
+
+    //     var tbody = document.createElement("tbody");
+
+    //     var linhasTabela = alunos.length;
+
+    //     for (var i = 0; i < linhasTabela; i++) {
+    //       var tr = document.createElement("tr");
+    //       var aluno = alunos[i];
+
+    //       //Mostrar nome do aluno
+    //       var td = document.createElement("td");
+    //       var txt = document.createTextNode(aluno.nome);
+    //       td.appendChild(txt);
+    //       tr.appendChild(td);
+
+    //       //Mostrar lista de matérias
+    //       var td = document.createElement("td");
+    //       const listaMaterias = document.createElement("ul");
+    //       listaMaterias.classList.add("list-group");
+    //       for (let i = 0; i < aluno.materias.length; i++) {
+    //         const itemMateria = document.createElement("li");
+    //         itemMateria.classList.add("list-group-item");
+    //         var txt = document.createTextNode(aluno.materias[i].nomeMateria);
+    //         itemMateria.appendChild(txt);
+    //         var sp = document.createTextNode(": ");
+    //         itemMateria.appendChild(sp);
+    //         var num = document.createTextNode(aluno.materias[i].faltas);
+    //         itemMateria.appendChild(num);
+    //         listaMaterias.appendChild(itemMateria);
+    //         td.appendChild(listaMaterias);
+    //         tr.appendChild(td);
+    //       }
+
+    //       tr.appendChild(td);
+    //       tbody.appendChild(tr);
+    //     }
+
+    //     table.appendChild(tbody);
+
+    //     main.appendChild(table);
+    //   });
   }
 
   btnFrequencia.onclick = () => {
     main.innerHTML = "";
     gerarFreq("http://localhost:3000/alunos");
   };
-  
 };
+
+window.addEventListener("DOMContentLoaded", () => renderPage());
