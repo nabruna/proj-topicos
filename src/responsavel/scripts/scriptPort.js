05508de7c76171775f1472a88e893fc4c310d8b1
fernandalimaa <fernanda.lima@thebrooklynbrothers.com>
2022-06-23 00:07:33 -0300

    parents finished

---
+++ src/responsavel/scripts/scriptPort.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $.getJSON("../shared/mockVidaEscolarr.json", function (obj) {
+      var tabela = "";
+      $.each(obj.alunos, function (i, value) {
+
+        tabela += '<td><ul class="list-group">';
+        
+        
+        $.each(value.atividadesPort, function (k, v) {
+            tabela += '<li class="list-group-item">' + v.nomeAtividade + '</li>';
+        });
+        tabela += '</ul></td>';
+  
+      });
+      $("#contPort").append(tabela);
+    });
+  }); 
