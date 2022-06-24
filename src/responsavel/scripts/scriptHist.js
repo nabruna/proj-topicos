bd386d4a5de5f4997fa745b8b5dc1cfa238179ef
fernandalimaa <fernanda.lima@thebrooklynbrothers.com>
2022-06-24 16:04:25 -0300

    the end

--- src/responsavel/scripts/scriptHist.js
+++ src/responsavel/scripts/scriptHist.js
@@ -10,6 +10,12 @@ $(document).ready(function () {
             tabela += '<li class="list-group-item">' + v.nomeAtividade + '</li>';
         });
         tabela += '</ul></td>';
+        tabela += '<td><ul class="list-group">';
+
+        $.each(value.agenda, function (k, v) {
+          tabela += '<li class="list-group-item">' + v.diaAtividade + '</li>';
+      });
+      tabela += '</ul></td>';
   
       });
       $("#contHist").append(tabela);
